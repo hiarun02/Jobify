c05a4fd3078f96cb37aae848f6b9c1334226b08a
hiarun01 <hiarun02@gmail.com>
2025-05-10 19:41:04 +0530

    fixed job application

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 //production, development
-const mode = "production";
+const mode = "development";
 
 // CORS configuration
 if (mode === "development") {
@@ -30,7 +30,7 @@ if (mode === "development") {
 } else if (mode === "production") {
   app.use(
     cors({
-      origin: "https://hi-jobify.vercel.app",
+      origin: "https://hi-jobify.vercel.app/",
       credentials: true,
     })
   );
